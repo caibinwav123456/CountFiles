ed6758f79e51536d84dd65bb8d552326dc094643
CaiBin <342571403@qq.com>
2023-09-19 13:11:31 +0800

    .

--- CountFiles/include/defines.h
+++ CountFiles/include/defines.h
@@ -1,36 +1,38 @@
 #ifndef _DEFINES_H_
 #define _DEFINES_H_
 
-#define LINE_HEIGHT 20
-#define LINE_INDENT 20
-#define TEXT_HEIGHT 16
-#define FOLDER_METRIC 24
-#define CONN_START 10
-#define CONN_END 20
-#define CONN_Y 10
-#define FILE_WIDTH 12
-#define MIN_SCROLL_WIDTH 294
-#define BAR_CENTER_MARGIN 2
-#define BAR_CENTER_SPACE 38
-#define FILE_RECT CRect(3,6,9,14)
-#define CONN_COLOR RGB(128,128,128)
-#define GREY_COLOR RGB(248,250,249)
-#define RED_COLOR RGB(255,0,0)
-#define BLUE_COLOR RGB(0,0,255)
-#define OLD_COLOR RGB(128,128,128)
-#define YELLOW_COLOR RGB(128,128,0)
-#define SEL_COLOR RGB(204,255,204)
-#define SELN_COLOR RGB(226,246,226)
-#define BACK_GREY_COLOR RGB(160,160,160)
-
-#define TAG_TYPE_DIR "D "
-#define TAG_TYPE_FILE "N "
-#define TAG_DRSIZE "DirRecSize: "
-#define TAG_SIZE "Size: "
-#define TAG_ERR_DESC "error: "
-
-#define dir_symbol '\\'
-
-#define VIEW_FONT _T("Arial")
+#define LINE_HEIGHT        20
+#define LINE_INDENT        20
+#define TEXT_HEIGHT        16
+#define FOLDER_METRIC      24
+#define CONN_START         10
+#define CONN_END           20
+#define CONN_Y             10
+#define FILE_WIDTH         12
+#define MIN_SCROLL_WIDTH   294
+#define BAR_CENTER_MARGIN  2
+#define BAR_CENTER_SPACE   38
+#define MIN_TABWIDTH_NAME  80
+#define MIN_TABWIDTH_OTHER 10
+#define FILE_RECT          CRect(3,6,9,14)
+#define CONN_COLOR         RGB(128,128,128)
+#define GREY_COLOR         RGB(248,250,249)
+#define RED_COLOR          RGB(255,0,0)
+#define BLUE_COLOR         RGB(0,0,255)
+#define OLD_COLOR          RGB(128,128,128)
+#define YELLOW_COLOR       RGB(128,128,0)
+#define SEL_COLOR          RGB(204,255,204)
+#define SELN_COLOR         RGB(226,246,226)
+#define BACK_GREY_COLOR    RGB(160,160,160)
+						   
+#define TAG_TYPE_DIR       "D "
+#define TAG_TYPE_FILE      "N "
+#define TAG_DRSIZE         "DirRecSize: "
+#define TAG_SIZE           "Size: "
+#define TAG_ERR_DESC       "error: "
+						   
+#define dir_symbol         '\\'
+						   
+#define VIEW_FONT          _T("Arial")
 
 #endif
