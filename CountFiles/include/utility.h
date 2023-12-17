8aed73a7c9bb351120b1f6dce6bf10c810ae6342
CaiBin <342571403@qq.com>
2023-12-17 13:22:10 +0800

    .

--- CountFiles/include/utility.h
+++ CountFiles/include/utility.h
@@ -69,4 +69,41 @@ int merge_ordered_list(iteratorT1 l1,iteratorT2 l2,int (*cb)(iteratorT1,iterator
 	}
 	return 0;
 }
+struct file_recurse_cbdata
+{
+	int (*cb)(int, char*, dword, void*, char);
+	void* param;
+};
+struct file_size
+{
+	uint sizeh;
+	uint sizel;
+};
+struct path_recurse_stat
+{
+	file_size size;
+	uint nfile;
+	uint ndir;
+};
+struct file_recurse_callback
+{
+	int (*_fstat_)(char* pathname, dword* type);
+	int (*_ftraverse_)(char* pathname, int(*cb)(char*, dword, void*, char), void* param);
+	int (*_mkdir_)(char* path);
+	int (*_fcopy_)(char* from, char* to);
+	int (*_fdelete_)(char* pathname);
+};
+struct file_recurse_handle_callback
+{
+	int (*_fstat_)(char* pathname, dword* type);
+	int (*_ftraverse_)(char* pathname, int(*cb)(char*, dword, void*, char), void* param);
+	int (*_handler_)(char* pathname, dword, void*, char);
+};
+DLLAPI(int) recurse_fcopy(char* from,char* to,file_recurse_callback* callback,file_recurse_cbdata* cbdata,char dsym);
+DLLAPI(int) recurse_fdelete(char* pathname,file_recurse_callback* callback,file_recurse_cbdata* cbdata,char dsym);
+DLLAPI(int) recurse_fhandle(char* pathname,file_recurse_handle_callback* callback,bool root_first,void* param,file_recurse_cbdata* cbdata,char dsym);
+DLLAPI(int) sys_recurse_fcopy(char* from,char* to,file_recurse_cbdata* cbdata);
+DLLAPI(int) sys_recurse_fdelete(char* pathname,file_recurse_cbdata* cbdata);
+DLLAPI(int) sys_recurse_fstat(char* pathname,path_recurse_stat* pstat,file_recurse_cbdata* cbdata);
+DLLAPI(void) random_integer64(UInteger64& num);
 #endif
