cff88721a44fe7f79adb9f51cef7a6491c62052b
CaiBin <342571403@qq.com>
2024-01-11 08:20:34 +0800

    .

--- CountFiles/FileCleaner/PropWnd.h
+++ CountFiles/FileCleaner/PropWnd.h
@@ -1,8 +1,7 @@
 #ifndef _PROP_WND_H_
 #define _PROP_WND_H_
 #include "DrawObject.h"
-class CPropWnd :
-	public CWnd
+class CPropWnd:public CWnd
 {
 	DECLARE_DYNAMIC(CPropWnd)
 	DECLARE_ID2WND_MAP(CPropWnd)
@@ -14,10 +13,10 @@ private:
 
 private:
 	DECLARE_MESSAGE_MAP()
-	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-	afx_msg LRESULT OnSizeParent(WPARAM wParam, LPARAM lParam);
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 	virtual void PostNcDestroy();
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg LRESULT OnSizeParent(WPARAM wParam, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
